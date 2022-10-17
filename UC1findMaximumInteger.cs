ba21d8cd827b71cefc7ae358ef0226e059121c14
SurveshRathore <educatedevilboy@gmail.com>
2022-10-17 19:00:14 +0530

    Find maximum among 3 Integers

---
+++ UC1findMaximumInteger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13FindMaximumUsingGenerics
+{
+    internal class UC1findMaximumInteger
+    {
+        public int findMaxInteger(int num1, int num2, int num3)
+        {
+            if ((num1.CompareTo(num2) > 0) && (num1.CompareTo(num3) > 0) )
+            {
+                return num1;
+            }
+
+            else if(num2.CompareTo(num3) >0)
+            {
+                return num2;
+            }
+            else
+            {
+               return num3;
+            }
+        }
+    }
+}
