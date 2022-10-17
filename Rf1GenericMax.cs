759a42ec26c111faab93e9c876482485411204a4
SurveshRathore <educatedevilboy@gmail.com>
2022-10-17 20:07:23 +0530

    Refactor all the 3 to One Generic Method and find the maximum

---
+++ Rf1GenericMax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13FindMaximumUsingGenerics
+{
+    internal class Rf1GenericMax
+    {
+        public T FindMaxUsingGenerics<T> (T value1, T value2, T value3) where T : IComparable
+        {
+            if ((value1.CompareTo(value2) > 0) && (value1.CompareTo(value3) > 0))
+            {
+                return value1;
+            }
+
+            else if (value2.CompareTo(value3) > 0)
+            {
+                return value2;
+            }
+            else
+            {
+                return value3;
+            }
+
+        }
+    }
+}
